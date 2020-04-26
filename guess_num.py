b706ec21326baa6c4846b8320e1299889ecfe549
susiexwater <64352439+susiexwater@users.noreply.github.com>
2020-04-26 17:21:45 +0800

    guess_num.py

---
+++ guess_num.py
@@ -0,0 +1,16 @@
+import random
+
+## test
+
+print('猜数字游戏')
+n = random.randint(0,100)
+
+while True:
+    ni =int(input('请输入要猜的数字： '))
+    if ni >n:
+        print('猜大了，请继续')
+    elif ni < n :
+        print('猜小了，请继续')
+    else:
+        print('恭喜猜对了')
+        break
